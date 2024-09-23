f4c1308796dd15001a0b70a759ce8fcc08da344f
Aphrodis1st <aphrodisu2019@gmail.com>
2024-09-23 18:47:50 +0200

    recently added image fix

--- src/components/common/RecentlyAddedCard.tsx
+++ src/components/common/RecentlyAddedCard.tsx
@@ -14,10 +14,11 @@ const RecentlyAddedCard = ({
   return (
     <div>
       <div className="relative">
-        <div className="h-[40vh]">
+        <div className="h-[40vh] relative">
           <img src={image} alt="image" className="h-full w-full object-cover" />
+          <div className="w-full h-[40vh] top-0 z-10 bg-black/30 absolute"></div>
         </div>
-        <div className="absolute top-0 px-5">
+        <div className="absolute z-20 top-0 px-5">
           <h1 className="text-2xl font-black">{title}</h1>
           <p className="text-base mt-2">
             {description.length > 12
@@ -25,7 +26,9 @@ const RecentlyAddedCard = ({
               : description}
           </p>
         </div>
-        <div className="absolute bottom-4 italic font-bold px-5">{owner}</div>
+        <div className="absolute z-20 bottom-4 italic font-bold px-5">
+          {owner}
+        </div>
       </div>
     </div>
   );
